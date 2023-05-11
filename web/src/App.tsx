97798dcafb601c76868671a9e13514b4b8a1f2f2
Fanuel Couto <fanuel.couto99@gmail.com>
2023-05-10 22:37:22 -0300

    Estilizando componente post

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -13,6 +13,7 @@ function App() {
 
         <main>
           <Post />
+          <Post />
         </main>
       </div>
     </div>
--- web/src/components/Post.tsx
+++ web/src/components/Post.tsx
@@ -1,10 +1,10 @@
 export function Post() {
     return (
-        <article className="">
-            <header>
+        <article className="post bg-gray-800 rounded-lg p-10">
+            <header className="">
                 <div className="">
-                    <img 
-                        src="https://github.com/fanuelcouto99.png" 
+                    <img
+                        src="https://github.com/fanuelcouto99.png"
                         className=""
                     />
                     <div className="">
@@ -18,11 +18,15 @@ export function Post() {
                 </time>
             </header>
 
-            <div className="">
-                <p>Fala galeraa 👋 </p>
-                <p>Acabei  de subir mais um projeto no meu portifólio. É um projeto que fiz no Ignite, evento da RocketSeat.</p>
-                <p>👉 <a href="#">github.com/fanuelcouto99</a></p>
-                <p>👉 <a href="#">#novoprojeto #ignite #rocketseat</a></p>
+            <div className="leading-8 text-gray-300 mt-6">
+                <p className="mt-4">Fala galeraa 👋 </p>
+                <p className="mt-4">Acabei  de subir mais um projeto no meu portifólio. É um projeto que fiz no Ignite, evento da RocketSeat.</p>
+                <p className="mt-4"><a href="#" className="font-bold text-green-500 hover:text-green-300">github.com/fanuelcouto99</a></p>
+                <p className="mt-4">
+                    <a href="#" className="font-bold text-green-500 hover:text-green-300">#novoprojeto</a>{' '}
+                    <a href="#" className="font-bold text-green-500 hover:text-green-300">#ignite</a>{' '}
+                    <a href="#" className="font-bold text-green-500 hover:text-green-300">#rocketseat</a>
+                </p>
             </div>
         </article>
     );

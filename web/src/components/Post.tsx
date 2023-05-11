3f45100564637be158ac8c1e10de59e0e34f55e4
Fanuel Couto <fanuel.couto99@gmail.com>
2023-05-10 21:40:05 -0300

    Finalizado estrutura do componente post

--- web/src/components/Post.tsx
+++ web/src/components/Post.tsx
@@ -13,10 +13,17 @@ export function Post() {
                     </div>
                 </div>
 
-                <time dateTime='2023-05-10 21:31:00'>
+                <time title='10 de Maio ás 21:31h' dateTime='2023-05-10 21:31:00'>
                     Publicado há 1h
                 </time>
             </header>
+
+            <div className="">
+                <p>Fala galeraa 👋 </p>
+                <p>Acabei  de subir mais um projeto no meu portifólio. É um projeto que fiz no Ignite, evento da RocketSeat.</p>
+                <p>👉 <a href="#">github.com/fanuelcouto99</a></p>
+                <p>👉 <a href="#">#novoprojeto #ignite #rocketseat</a></p>
+            </div>
         </article>
     );
 };
